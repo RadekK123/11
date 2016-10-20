babb6d68909129fcb9ccf5eed4a4c320a9d0ce05
Radek Knapkiewicz <radknap@gmail.com>
2016-10-20 15:55:14 +0200

    Joke Chuck Norris

--- 11.1/js/first.js
+++ 11.1/js/first.js
@@ -19,26 +19,6 @@ function getJoke () {
 	xhr.send();
 }
 
-/*
-var url = 'http://api.icndb.com/jokes/random';
-
-var $button = $('#get-joke').click(function () {
-	getJoke();
-});
-
-var $paragraph = $('#joke');
-
-function getJoke() {
-	$.ajax({
-		url: url,
-		method: 'GET',
-		success: function (res) {
-			$paragraph.text(res.value.joke);
-		},
-
-	});
-}
-*/
 
 
 
